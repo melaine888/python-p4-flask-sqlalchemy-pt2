5325c359f70587c8b6e22bdd7143c27016d4a48b
Melaine Kanana <melaine.kanana@student.moringaschool.com>
2023-06-23 10:08:37 +0300

    completed assignment

--- server/seed.py
+++ server/seed.py
@@ -6,3 +6,27 @@ from faker import Faker
 
 from app import app
 from models import db, Owner, Pet
+
+
+faker = Faker()
+
+with app.app_context():
+    Pet.query.delete()
+    Owner.query.delete()
+    
+    owners = []
+    for n in range(50):
+        owner = Owner(name = faker.name())
+        owners.append(owner)
+        
+    db.session.add_all(owners)
+    
+    pets = []
+    species = ['Dog' , 'Cat ','Chicken' , 'Hamster' , 'Turtle']
+    for n in range(100):
+        pet = Pet(name=fakerO.first_name(), species=rc(species), owner=rc(owners))
+        pets.append(pet)
+        
+    db.session.add_all(pets)
+    db.session.commit()
+    
